c64154acdda935a3c6678016c22f51914dcdc41c
SanghunKwan <165416919+SanghunKwan@users.noreply.github.com>
2025-06-17 16:52:52 +0900

    카드 선택 및 삭제 추가

---
+++ Assets/2. Scripts/Managers/IngameManager.cs
@@ -0,0 +1,183 @@
+using System.Collections.Generic;
+using UnityEngine;
+using DefineEnum;
+using System.Collections;
+
+public class IngameManager : MonoBehaviour
+{
+    static IngameManager _uniqueInstance;
+
+    const int _limitWidth = 6;
+    Vector2 _offset = new Vector2(1.6f, -2.1f);
+
+
+    //참조 변수
+    GameObject _prefabCard;
+    Transform _cardRoot;
+
+    //정보 변수
+    int _firstIndex = -1;
+    int _secondIndex = -1;
+    int _choiceCount;
+
+    //Datas
+    List<CardObj> _genCardList;
+    public bool _isChoiceEnd => _choiceCount == 2;
+
+    public static IngameManager _instance => _uniqueInstance;
+
+    private void Awake()
+    {
+        _uniqueInstance = this;
+    }
+    private void Start()
+    {
+        _prefabCard = Resources.Load<GameObject>("Prefabs/Objects/CardObject");
+        GameObject go = GameObject.FindGameObjectWithTag("PosRoot");
+        _cardRoot = go.transform;
+        StartCoroutine(GenerateCard(30, 0.10f));
+    }
+    private void Update()
+    {
+        if (_choiceCount == 2)
+        {
+            if (_genCardList[_firstIndex]._isOpened && _genCardList[_secondIndex]._isOpened)
+            {
+                if (MatchedCard(_firstIndex, _secondIndex))
+                {
+                    //삭제
+                    Debug.LogFormat("{0}번 카드와 {1}번 카드가 삭제되었습니다.", _firstIndex, _secondIndex);
+
+                    Destroy(_genCardList[_firstIndex].gameObject);
+                    Destroy(_genCardList[_secondIndex].gameObject);
+
+                    _choiceCount = 0;
+                    _firstIndex = _secondIndex = -1;
+                }
+                else
+                {
+                    _genCardList[_firstIndex].CloseCard();
+                    _genCardList[_secondIndex].CloseCard();
+                }
+            }
+            else if (_genCardList[_firstIndex]._isClosed && _genCardList[_secondIndex]._isClosed)
+            {
+                _choiceCount = 0;
+                _firstIndex = _secondIndex = -1;
+            }
+        }
+    }
+
+    //카드 관련 함수 모음
+    IEnumerator GenerateCard(int count, float delay)
+    {
+        _genCardList = new List<CardObj>();
+        int[] iconNumbers = GetShuffleCard(count / 2);
+
+        int idxX;
+        int idxY;
+        Vector2 pos;
+        GameObject go;
+        CardObj obj;
+
+        for (int i = 0; i < count; i++)
+        {
+            idxX = i % _limitWidth;
+            idxY = i / _limitWidth;
+            pos = new Vector2(idxX * _offset.x, idxY * _offset.y);
+
+            go = Instantiate(_prefabCard, pos, Quaternion.identity, _cardRoot);
+            go.transform.localPosition = pos;
+            obj = go.GetComponent<CardObj>();
+            obj.InitSetData(i, CardBGKind.CARD_BG_SUN, (CardIconType)iconNumbers[i]);
+            _genCardList.Add(obj);
+
+            yield return new WaitForSeconds(delay);
+        }
+    }
+    int[] GetShuffleCard(int typeCount)
+    {
+        int[] returnCards = new int[typeCount * 2];
+        int[] typeIndexList = new int[typeCount];
+
+        int cnt = (int)CardIconType.Max;
+        // typeCount만큼의 종류를 임의로 뽑는다.
+        // 1.
+        //for (int i = 0; i < typeIndexList.Length; i++)
+        //{
+        //    int number = Random.Range(0, cnt);
+        //    int index = i;
+        //    while (--index >= 0)
+        //    {
+        //        if (typeIndexList[index] == number)
+        //        {
+        //            number = Random.Range(0, cnt);
+        //            index = i;
+        //        }
+        //    }
+        //    typeIndexList[i] = number;
+        //}
+
+
+        //2.
+        int[] typeList = new int[cnt];
+        for (int i = 0; i < cnt; i++)
+            typeList[i] = i;
+
+        int mix = 3;
+        for (int i = 0; i < mix; i++)
+        {
+            for (int j = 0; j < cnt; j++)
+            {
+                int index = Random.Range(0, cnt);
+                int number = typeList[i];
+                typeList[i] = typeList[index];
+                typeList[index] = number;
+            }
+        }
+
+        for (int i = 0; i < typeIndexList.Length; i++)
+        {
+            int index = i * 2;
+            typeIndexList[i] = typeList[i];
+            returnCards[index] = typeIndexList[i];
+            returnCards[index + 1] = typeIndexList[i];
+        }
+        mix = 5;
+        for (int i = 0; i < mix; i++)
+        {
+            for (int j = 0; j < returnCards.Length; j++)
+            {
+                int index = Random.Range(0, returnCards.Length);
+                int number = returnCards[i];
+                returnCards[i] = returnCards[index];
+                returnCards[index] = number;
+            }
+        }
+
+        return returnCards;
+    }
+    bool MatchedCard(int firstIndex, int secondIndex)
+    {
+        if (_genCardList[firstIndex]._cardType != _genCardList[secondIndex]._cardType) return false;
+
+        return true;
+    }
+
+    public void SelectCard(int NO)
+    {
+        if (_firstIndex == -1)
+        {
+            _firstIndex = NO;
+            _choiceCount = 1;
+        }
+        else if (_secondIndex == -1)
+        {
+            if (_firstIndex != _secondIndex)
+            {
+                _secondIndex = NO;
+                _choiceCount = 2;
+            }
+        }
+    }
+}
--- Assets/2. Scripts/Objects/CardObj.cs
+++ Assets/2. Scripts/Objects/CardObj.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using DefineEnum;
+using System;
 
 public class CardObj : MonoBehaviour
 {
@@ -10,21 +11,137 @@ public class CardObj : MonoBehaviour
 
     int _myNO;
     CardIconType _myType;
+    public CardIconType _cardType => _myType;
+    bool _isFront;
+    bool _isRolling;
+    public bool _isOpened => (!_isRolling) && _isFront;
+    public bool _isClosed => (!_isRolling) && (!_isFront);
+
+    Transform tr;
+    [SerializeField] float rollingSpeed;
 
 
     //임시
     private void Start()
     {
-        InitSetData(1, CardBGKind.CARD_BG_VISION, CardIconType.Icon11);
+        tr = transform;
     }
+    private void Update()
+    {
+        if (_isRolling)
+        {
+            float rollingDelta = rollingSpeed * Time.deltaTime;
+            float angleY = tr.rotation.eulerAngles.y;
+            float pageHalf = Convert.ToInt32(!_isFront) * 180;
+
+            tr.Rotate(Vector3.up, rollingDelta);
+
+            if (angleY > 90 + pageHalf)
+                _frontSide.SetActive(_isFront);
+
+            if (_isFront ? (angleY > 180) : (angleY < 180))
+            {
+                _isRolling = false;
+                tr.rotation = Quaternion.Euler(0, pageHalf + 180, 0);
+            }
+
+            ////(rollingDelta / 2) 변수로.
+            //if (_isFront)
+            //{
+            //    if (tr.rotation.eulerAngles.y > 90)
+            //        _frontSide.SetActive(true);
+
+            //    if (tr.rotation.eulerAngles.y > 180)
+            //    {
+            //        _isRolling = false;
+            //        tr.rotation = Quaternion.Euler(0, 180, 0);
+            //    }
+            //}
+            //else
+            //{
+            //    if (tr.rotation.eulerAngles.y > 270)
+            //        _frontSide.SetActive(false);
+
+            //    if (tr.rotation.eulerAngles.y < 180)
+            //    {
+            //        _isRolling = false;
+            //        tr.rotation = Quaternion.Euler(0, 0, 0);
+            //    }
+            //}
+
+
+
+
+            //if (angleY > 90 - (rollingDelta / 2) && angleY < 90 + (rollingDelta / 2))
+            //{
+            //    _isFront = !_isFront;
+            //    _frontSide.SetActive(_isFront);
+            //}
+            //else if (angleY > 180 - (rollingDelta / 2) || angleY < (rollingDelta / 2))
+            //{
+            //    _isRolling = false;
+            //    tr.rotation = Quaternion.Euler(0, Mathf.Ceil(tr.rotation.eulerAngles.y), 0);
+            //}
+
+
+            //if (_isFront == _wasFront)
+            //{
+            //    //회전
+            //    if (tr.rotation.eulerAngles.y < 89)
+            //    {
+            //        tr.Rotate(Vector3.up, rollingDelta);
+            //    }
+            //    else
+            //    {
+            //        _isFront = !_isFront;
+            //        _frontSide.SetActive(_isFront);
+            //    }
+            //}
+            //else
+            //{
+            //    if (tr.rotation.eulerAngles.y > rollingDelta)
+            //    {
+            //        tr.Rotate(Vector3.up, -rollingDelta);
+            //    }
+            //    else
+            //    {
+            //        _isRolling = false;
+            //        tr.rotation = Quaternion.Euler(0, 0, 0);
+            //    }
+            //}
+        }
+    }
+
     public void InitSetData(int no, CardBGKind bg, CardIconType type)
     {
         //Transform tf = transform;
         //_frontSide = tf.GetChild(1).gameObject;
         //_icon = _frontSide.transform.GetChild(0).GetComponent<SpriteRenderer>();
         _myNO = no;
+        _myType = type;
         _cardBG.sprite = ResourcePoolManager._instance.Get<Sprite>(PoolDataType.CARDIMAGEBG, (int)bg);
         _icon.sprite = ResourcePoolManager._instance.Get<Sprite>(PoolDataType.CARDIMAGEICON, (int)type);
+        _frontSide.SetActive(false);
+        _isFront = false;
+        _isRolling = false;
+    }
 
+    public void OpenCard()
+    {
+        _isRolling = true;
+        _isFront = true;
+    }
+    public void CloseCard()
+    {
+        _isRolling = true;
+        _isFront = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (_isRolling || IngameManager._instance._isChoiceEnd) return;
+
+        OpenCard();
+        IngameManager._instance.SelectCard(_myNO);
     }
 }
